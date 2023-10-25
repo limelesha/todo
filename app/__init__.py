c09b8f49a83ef96118fe92e4719b88539e7609ad
Alaksiej Kanstancinau <konstantinov.a.y2005@gmail.com>
2023-10-25 12:48:55 +0200

    API improvements

--- app/__init__.py
+++ app/__init__.py
@@ -10,198 +10,289 @@ from . import models
 app = flask.Flask(__name__)
 app.secret_key = secrets.token_hex()
 
-database = models.Database("sqlite:///:memory:")
-
-
-# def unpack_request_body(func):
-#     @functools.wraps(func)
-#     def wrapper(*args, **kwargs):
-#         return func(*args, **kwargs, **flask.request.get_json)
-#     return wrapper
-
-
-def update_instance_from_dict(obj, values: dict) -> None:
-    for key, value in values.items():
-        setattr(obj, key, value)
+database = models.Database("sqlite:///data.db")
 
 
 def require_login(func):
-    """Decorator that checks whether a user is logged in."""
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
         if 'user_id' in flask.session:
             return func(*args, **kwargs)
-        flask.abort(403)  # TODO: otherwise, redirect to login page
-    return wrapper
-
-
-def require_self(func):
-    """Decorator that ensures that user related actions are performed only by that user."""
-    @functools.wraps(func)
-    def wrapper(*, user_id, **kwargs):
-        if flask.session.user_id == user_id:
-            return func(user_id=user_id, **kwargs)
         else:
-            flask.abort(403)
+            flask.abort(401)
     return wrapper
 
 
-def require_access_level(required_access_level: models.AccessLevel):
-    """Decorator that ckecks whether a user has a sufficient access level to perform a project related action."""
-    def decorator(func):
-        @functools.wraps(func)
-        def wrapper(*, project_id, **kwargs):
-            with database.new_session() as session:
-                membership = session.scalar(
-                    sqlalchemy.select(models.Membership)
-                    .where(
-                        sqlalchemy.and_(
-                            models.Membership.user_id == flask.session.user_id,
-                            models.Membership.project_id == project_id
-                        )
-                    )
-                )
-                if membership is None:
-                    flask.abort(403)  # not a member
-                user_access_level = membership.access_level
-                if user_access_level == required_access_level:
-                    return func(project_id=project_id, **kwargs)
-        return wrapper
-    return decorator
-
-
-# having at least some access level means being a member
-# so, membership requirement is identical to a requirement of having at least the lowest access level
-require_member = require_access_level(models.AccessLevel.READER)
-
-
-@app.get('/task/<int:task_id>')
-def get_task(*, task_id: int):
+def get_access_level(user, project) -> models.AccessLevel:
+    statement = (
+        sqlalchemy.select(models.Membership)
+        .where(
+            sqlalchemy.and_(
+                models.Membership.user == user,
+                models.Membership.project == project
+            )
+        )
+    )
     with database.new_session() as session:
-        task = session.get(models.Task, task_id)
-        if session.get(models.User, flask.session.user_id) not in task.project.team:
-            flask.abort(403)
-        return task.as_dict()
+        membership = session.scalar(statement)
+    if membership is None:
+        flask.abort(403)  # not a member
+    return membership.access_level
 
 
-@app.delete('/task/<int:task_id>')
-def delete_task(*, task_id: int):
-    with database.new_session() as session:
-        session.delete(session.get(models.Task, task_id))
-        session.commit()
-    return "OK", 200
+# TODO:
+# - validate input data in create and update methods with some validation
+#   library like marshmallow
+# - validate email in user creation and update methods
+# - remove projects when there is no one left in the team
+# - when querying from the database, use a method other than `get` which will
+#   raise an exception when the record cannot be found instead of returning
+#   None, then learn how to handle such exceptions to abort with 404
+# - add `leave` method that would allows members of the team to leave without
+#   others' permission
 
 
-@app.post('/task/')
-def create_task(project_id: int,
-                supertask_id: int | None = None,
-                title: str | None = None,
-                description: str | None = None,
-                due_timestamp: float| None = None):
-    task = models.Task(project_id=project_id,
-                       supertask_id=supertask_id,
-                       title=title,
-                       description=description,
-                       due_timestamp=due_timestamp)
-    with database.new_session() as session:
-        session.add(task)
-        session.commit()
-    return "OK", 200
-
-
-@app.patch('/task/<int:task_id>')
-def update_task(*, task_id: int):
-    with database.new_session() as session:
-        task = session.scalar(sqlalchemy.select(models.Task).where(models.Task.id == task_id))
-        update_instance_from_dict(task, flask.request.json)
-        session.commit()
-    return "OK", 200
-
-
-@app.get('/project/<int:project_id>')
-@require_member
-def get_project(*, project_id: int):
-    with database.new_session() as session:
-        project = session.get(models.Project, project_id)
-        return project.as_dict()
-
-
-@app.patch('/project/<int:project_id>')
-@require_access_level(models.AccessLevel.MANAGER)
-def update_project(*, project_id: int):
-    with database.new_session() as session:
-        project = session.scalar(sqlalchemy.select(models.Project).where(models.Project.id == project_id))
-        update_instance_from_dict(project, flask.request.json)
-        session.commit()
-    return "OK", 200
-
-
-@app.delete('/project/<int:project_id>')
-@require_access_level(models.AccessLevel.MANAGER)
-def delete_project(*, project_id: int):
-    with database.new_session() as session:
-        session.delete(session.get(models.Project, project_id))
-        session.commit()
-    return "OK", 200
-
-
-@app.get('/user/<int:user_id>')
-def get_user(*, user_id: int):
-    with database.new_session() as session:
-        user = session.get(models.User, user_id)
-        return user.as_dict()
-
-
-@app.post('/user/')
-def create_user(name: str, email: str, cleartext_password: str):
-    # TODO: verify email
-    user = models.User.create_with_cleartext_password(name=name,
-                                                      email=email,
-                                                      cleartext_password=cleartext_password)
-    with database.new_session() as session:
-        session.add(user)
-        session.commit()
-    return "OK", 200
-
-
-@app.patch('/user/<int:user_id>')
-@require_self
-def update_user(*, user_id: int):
+@app.post('/login/')
+def login():
     with database.new_session() as session:
         user = session.scalar(
             sqlalchemy.select(models.User)
-            .where(models.User.id == user_id)
+            .where(models.User.email == flask.request.form['email'])
         )
-        update_instance_from_dict(user, flask.request.json)
-        session.commit()
-    return "OK", 200
+        if user is None:
+            flask.abort(404)
+        if user.verify_password(flask.request.form['password']):
+            flask.session['user_id'] = user.id
+            return "OK", 200
+        else:
+            flask.abort(403)
 
 
-@app.delete('/user/<int:user_id>')
-@require_self
-def delete_user(*, user_id: int):
+# task-related api
+
+@app.post('/task/')
+@require_login
+def create_task():
     with database.new_session() as session:
-        session.delete(session.get(models.User, user_id))
+        project = session.get(models.Project, flask.request.json['project_id'])
+        client_user = session.get(models.User, flask.session['user_id'])
+        if project is None or client_user is None:
+            flask.abort(404)
+        if get_access_level(client_user, project) >= models.AccessLevel.EDITOR:
+            task = models.Task(**flask.request.json)
+            session.add(task)
+            session.flush()
+            repr = task.full_repr()
+            session.commit()
+        else:
+            flask.abort(403)
+    return repr
+
+
+@app.get('/task/<int:task_id>')
+@require_login
+def get_task(task_id: int):
+    with database.new_session() as session:
+        client_user = session.get(models.User, flask.session['user_id'])
+        task = session.get(models.Task, task_id)
+        if client_user is None or task is None:
+            flask.abort(404)
+        if client_user in task.project.team:
+            return task.full_repr()
+        else:
+            flask.abort(403)
+
+
+@app.patch('/task/<int:task_id>')
+@require_login
+def update_task(task_id: int):
+    with database.new_session() as session:
+        client_user = session.get(models.User, flask.session['user_id'])
+        task = session.get(models.Task, task_id)
+        if client_user is None or task is None:
+            flask.abort(404)
+        if get_access_level(client_user, task.project) >= models.AccessLevel.EDITOR:
+            assert isinstance(flask.request.json, dict)
+            for key, value in flask.request.json.items():
+                if key in ['title', 'description', 'due_timestamp', 'is_done']:
+                    setattr(task, key, value)
+            session.flush()
+            repr = task.full_repr()
+            session.commit()
+            return repr
+
+
+@app.delete('/task/<int:task_id>')
+@require_login
+def delete_task(task_id: int):
+    with database.new_session() as session:
+        client_user = session.get(models.User, flask.session['user_id'])
+        task = session.get(models.Task, task_id)
+        if client_user is None or task is None:
+            flask.abort(404)
+        if get_access_level(client_user, task.project) >= models.AccessLevel.EDITOR:
+            session.delete(task)
+            session.commit()
+            return "OK", 200
+        else:
+            flask.abort(403)
+
+
+# project-related api
+
+@app.post('/project/')
+@require_login
+def create_project():
+    with database.new_session() as session:
+        client_user = session.get(models.User, flask.session['user_id'])
+        if client_user is None:
+            flask.abort(404)
+        project = models.Project(**flask.request.json)
+        membership = models.Membership(
+            user=client_user,
+            project=project,
+            access_level_int=models.AccessLevel.MANAGER.real
+        )
+        session.add_all([project, membership])
+        session.flush()
+        repr = project.full_repr()
+        session.commit()
+    return repr
+
+
+@app.get('/project/<int:project_id>')
+@require_login
+def get_project(project_id: int):
+    with database.new_session() as session:
+        client_user = session.get(models.User, flask.session['user_id'])
+        project = session.get(models.Project, project_id)
+        if client_user is None or project is None:
+            flask.abort(404)
+        if client_user in project.team:
+            return project.full_repr()
+        else:
+            flask.abort(403)
+
+
+@app.patch('/project/<int:project_id>')
+def update_project(project_id: int):
+    with database.new_session() as session:
+        client_user = session.get(models.User, flask.session['user_id'])
+        project = session.get(models.Project, project_id)
+        if client_user is None or project is None:
+            flask.abort(404)
+        if get_access_level(client_user, project) >= models.AccessLevel.MANAGER:
+            assert isinstance(flask.request.json, dict)
+            for key, value in flask.request.json.items():
+                if key in ['title', 'default_access_level_int']:
+                    setattr(project, key, value)
+            session.flush()
+            repr = project.full_repr()
+            session.commit()
+            return repr
+
+
+@app.delete('/project/<int:project_id>')
+@require_login
+def delete_project(*, project_id: int):
+    with database.new_session() as session:
+        client_user = session.get(models.User, flask.session['user_id'])
+        project = session.get(models.Project, project_id)
+        if client_user is None or project is None:
+            flask.abort(404)
+        if get_access_level(client_user, project) >= models.AccessLevel.MANAGER:
+            session.delete(project)
+            session.commit()
+            return "OK", 200
+
+
+# user-related api
+
+@app.post('/user/')
+def create_user():
+    # TODO: verify email
+    with database.new_session() as session:
+        user = models.User.create_with_cleartext_password(**flask.request.json)
+        session.add(user)
+        session.flush()
+        repr = user.full_repr()
+        session.commit()
+    return repr
+
+
+@app.get('/user/<int:user_id>')
+@require_login
+def get_user(*, user_id: int):
+    with database.new_session() as session:
+        target_user = session.get(models.User, user_id)
+        if target_user is None:
+            flask.abort(404)
+        return target_user.full_repr()
+
+
+@app.patch('/user/')
+@require_login
+def update_user():
+    with database.new_session() as session:
+        client_user = session.get(models.User, flask.session['user_id'])
+        if client_user is None:
+            flask.abort(404)
+        assert isinstance(flask.request.json, dict)
+        for key, value in flask.request.json.items():
+            if key in ['name', 'email']:
+                setattr(client_user, key, value)
+        session.flush()
+        repr = client_user.full_repr()
+        session.commit()
+    return repr
+
+
+@app.delete('/user/')
+@require_login
+def delete_user():
+    with database.new_session() as session:
+        client_user = session.get(models.User, flask.session['user_id'])
+        if client_user is None:
+            flask.abort(404)
+        del flask.session['user_id']
+        session.delete(client_user)
         session.commit()
     return "OK", 200
 
+
+# project-related actions api
 
 @app.post('/invite/<int:project_id>')
-@require_access_level(models.AccessLevel.MANAGER)
-def invite(*, project_id: int):
+@require_login
+def invite(project_id: int):
     with database.new_session() as session:
+        client_user = session.get(models.User, flask.session['user_id'])
         project = session.get(models.Project, project_id)
-        party = [session.get(models.User, user_id) for user_id in flask.request.json]
-        project.team.update(party)
-        session.commit()
-    return "OK", 200
+        if client_user is None or project is None:
+            flask.abort(404)
+        if get_access_level(client_user, project) >= models.AccessLevel.MANAGER:
+            assert isinstance(flask.request.json, list)
+            party = [session.get(models.User, user_id) for user_id in flask.request.json]
+            project.team.update(party)
+            session.flush()
+            repr = project.full_repr()
+            session.commit()
+            return repr
 
 
 @app.post('/kick/<int:project_id>')
-@require_access_level(models.AccessLevel.MANAGER)
-def kick(*, project_id: int):
+@require_login
+def kick(project_id: int):
     with database.new_session() as session:
+        client_user = session.get(models.User, flask.session['user_id'])
         project = session.get(models.Project, project_id)
-        party = [session.get(models.User, user_id) for user_id in flask.request.json]
-        project.team.difference_update(party)
-    return "OK", 200
+        if client_user is None or project is None:
+            flask.abort(404)
+        if get_access_level(client_user, project) >= models.AccessLevel.MANAGER:
+            assert isinstance(flask.request.json, list)
+            party = [session.get(models.User, user_id) for user_id in flask.request.json]
+            project.team.difference_update(party)
+            session.flush()
+            repr = project.full_repr()
+            session.commit()
+            return repr
--- app/models.py
+++ app/models.py
@@ -28,33 +28,52 @@ class Task(Base):
     """A single task entry in a project."""
     __tablename__ = 'task'
 
+    # internal exposed fields
     id: Mapped[int] = orm.mapped_column(primary_key=True)
+
+    # userland relations fields
+    project_id: Mapped[int] = orm.mapped_column(sqlalchemy.ForeignKey("project.id"))
+    supertask_id: Mapped[int | None] = orm.mapped_column(sqlalchemy.ForeignKey("task.id"))
+
+    # userland raw data fields
     title: Mapped[str] = orm.mapped_column()
     description: Mapped[str | None] = orm.mapped_column()
     due_timestamp: Mapped[float | None] = orm.mapped_column()
-    project_id: Mapped[int] = orm.mapped_column(sqlalchemy.ForeignKey("project.id"))
-    supertask_id: Mapped[int | None] = orm.mapped_column(sqlalchemy.ForeignKey("task.id"))
     is_done: Mapped[bool] = orm.mapped_column(default=False)
 
+    # orm relationships
     project: Mapped["Project"] = orm.relationship(back_populates="tasks")
     subtasks: Mapped[list["Task"]] = orm.relationship(back_populates="supertask")
     supertask: Mapped["Task"] = orm.relationship(back_populates="subtasks", remote_side=[id])
 
     def __repr__(self):
         return f"Task(id={self.id!r}"\
+               f", project_id={self.project_id!r}"\
+               f", supertask_id={self.supertask_id!r}"\
                f", title={self.title!r}"\
                f", description={(self.description and textwrap.shorten(self.description, 24))!r}"\
                f", due_timestamp={self.due_timestamp!r}"\
-               f", project_id={self.project_id!r}"\
-               f", supertask_id={self.supertask_id!r}"\
                f", is_done={self.is_done!r})"
 
-    def as_dict(self) -> dict:
+    def shallow_repr(self) -> dict:
         return {
+            "id": self.id,
             "title": self.title,
             "description": self.description,
             "due_timestamp": self.due_timestamp,
-            "subtasks": self.subtasks
+            "is_done": self.is_done,
+            "subtasks": [task.as_dict() for task in self.subtasks],
+        }
+
+    def full_repr(self) -> dict:
+        return {
+            "id": self.id,
+            "project_id": self.project_id,
+            "title": self.title,
+            "description": self.description,
+            "due_timestamp": self.due_timestamp,
+            "is_done": self.is_done,
+            "subtasks": [task.as_dict() for task in self.subtasks],
         }
 
 
@@ -62,38 +81,53 @@ class Project(Base):
     """Project is a container of tasks."""
     __tablename__ = 'project'
 
+    # internal exposed fields
     id: Mapped[int] = orm.mapped_column(primary_key=True)
+
+    # userland raw data fields
     title: Mapped[str] = orm.mapped_column()
     default_access_level_int: Mapped[int] = orm.mapped_column(default=AccessLevel.READER.real)
 
+    # orm relationships
     tasks: Mapped[list["Task"]] = orm.relationship(back_populates="project")
     team: Mapped[set["Membership"]] = orm.relationship(back_populates="project")
 
     def __repr__(self):
         return f"Project(id={self.id!r}"\
-               f", title={self.title!r})"
+               f", title={self.title!r})"\
+               f", default_access_level={AccessLevel(self.default_access_level_int)}"
 
-    def as_dict(self) -> dict:
+    def shallow_repr(self) -> dict:
         return {
+            "id": self.id,
             "title": self.title,
-            "default_access_level_int": self.default_access_level_int,
-            "tasks": [task.as_dict for task in self.tasks]
         }
 
-    # def remove_done(self) -> None:
-    #     """Remove all tasks marked as done."""
-    #     self[:] = [task for task in self if not task.is_done]
+    def full_repr(self) -> dict:
+        return {
+            "id": self.id,
+            "title": self.title,
+            "default_access_level_int": self.default_access_level_int,
+            "tasks": [task.shallow_repr() for task in self.tasks],
+            "team": [user.shallow_repr() for user in self.team],
+        }
 
 
 class User(Base):
     """Registered user of the app."""
     __tablename__ = 'user'
 
+    # internal exposed fields
     id: Mapped[int] = orm.mapped_column(primary_key=True)
-    name: Mapped[str] = orm.mapped_column()
-    email: Mapped[str] = orm.mapped_column()
+
+    # internal secret fields
     password_hash: Mapped[str] = orm.mapped_column()
 
+    # userland raw data fields
+    name: Mapped[str] = orm.mapped_column()
+    email: Mapped[str] = orm.mapped_column(unique=True)
+
+    # orm relationships
     projects: Mapped[list["Membership"]] = orm.relationship(back_populates="user")
 
     _hasher: typing.ClassVar[argon2.PasswordHasher] = argon2.PasswordHasher()
@@ -105,7 +139,7 @@ class User(Base):
                f", password_hash={self.password_hash!r})"
 
     @classmethod
-    def create_with_cleartext_password(cls, name: str, email: str, cleartext_password: str):
+    def create_with_cleartext_password(cls, name: str, email: str, cleartext_password: str) -> typing.Self:
         """Create a user with a cleartext password.
 
         This method hashes the password and creates a new user object
@@ -115,16 +149,37 @@ class User(Base):
         return cls(name=name, email=email, password_hash=password_hash)
 
     @classmethod
-    def create_dummy(cls, name: str):
+    def create_dummy(cls, name: str) -> typing.Self:
         """Create a dummy user for debugging."""
         email = name.lower() + '@example.com'
         passwd = name.lower() + '123'
         return cls.create_with_cleartext_password(name=name, email=email, cleartext_password=passwd)
 
-    def as_dict(self):
+    def verify_password(self, cleartext_password: str) -> bool:
+        try:
+            self._hasher.verify(self.password_hash, cleartext_password)
+        except argon2.exceptions.VerifyMismatchError:
+            return False
+        except argon2.exceptions.VerificationError:
+            pass  # verification failed due to an unknown reason, should not happen
+        except argon2.exceptions.InvalidHashError:
+            pass  # hash is so clearly invalid, that it couldn’t be passed to Argon2
+        if self._hasher.check_needs_rehash(self.password_hash):
+            self.password_hash = self._hasher.hash(cleartext_password)
+        return True
+
+    def shallow_repr(self) -> dict:
         return {
+            "id": self.id,
             "name": self.name,
-            "projects": [project.id for project in self.projects]
+        }
+
+    def full_repr(self) -> dict:
+        return {
+            "id": self.id,
+            "name": self.name,
+            "email": self.email,
+            "projects": [project.shallow_repr() for project in self.projects]
         }
 
 
